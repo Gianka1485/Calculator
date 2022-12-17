2f8dd54fd0bd371cbc00d17de0feb7f00d96e2a8
Gianka1485 <gianfranco1485@gmail.com>
2022-12-17 01:19:34 -0500

    Incremented responsive width resolution

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,7 +4,7 @@ import '../styles/Display.css'
 const Display = ({ result }) => {
     const font = (s, l) => result.length >= 18 ? s : l;
     return (
-        <div className="display" style={window.innerWidth <= 280 ? {fontSize: font('1.1em', '1.5em')} : {fontSize: font('1.75em', '2em')} }>{result}</div>
+        <div className="display" style={window.innerWidth <= 400 ? {fontSize: font('1.1em', '1.5em')} : {fontSize: font('1.75em', '2em')} }>{result}</div>
     );
 }
 
