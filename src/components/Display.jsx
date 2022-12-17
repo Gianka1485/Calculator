51a59c0957b0d466003d34f88382b4342f8f69b7
Gianka1485 <gianfranco1485@gmail.com>
2022-12-17 00:38:44 -0500

    Responsive design added

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import '../styles/Display.css'
 
-const Display = ({ result }) => (
-    <div className="display" style={{fontSize: `${result.length >= 19 ? '1.75em' : '2em' }`}}>{result}</div>
-);
+const Display = ({ result }) => {
+    const font = (s, l) => result.length >= 18 ? s : l;
+    return (
+        <div className="display" style={window.innerWidth <= 280 ? {fontSize: font('1.1em', '1.5em')} : {fontSize: font('1.75em', '2em')} }>{result}</div>
+    );
+}
 
 export default Display;
